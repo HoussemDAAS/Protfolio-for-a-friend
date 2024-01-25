bab29b83bb35662dbdf77737d401195d4a4858ca
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-26 00:08:57 +0100

    sending email using emailJS

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import './Contact.css'
 import Titles from '../../components/Titles/Titles'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_h6rzrze", "template_jfgc93m", form.current, 'gLloXFr_UMLjuZ0CN')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div className='contact'>
         <Titles title={'CONTACT'} />
-        <form action="submit" className='contact-form'>
-        <input type="text" placeholder='Name' class="input"/>
-        <input type="email" placeholder='ENTER YOUR EMAIL'class="input" />
-        <input type="number" placeholder='PHONE NUMBER' class="input"/>
-        <textarea name="" id="" cols="30" rows="10" placeholder='Your Message'class="input">
+        <form  ref={form} onSubmit={sendEmail} className='contact-form'>
+        <input type="text" name="from_name" placeholder='Name' className="input"/>
+        <input type="email" name="user_email" placeholder='ENTER YOUR EMAIL'className="input" />
+        <input type="number" name='phone' placeholder='PHONE NUMBER' className="input"/>
+        <textarea  name="message" id="" cols="30" rows="10" placeholder='Your Message'className="input">
              
         </textarea>
 
-        <button type='submit'>SUBMIT</button>
+        <button  type="submit" value="Send">SUBMIT</button>
 
 
         </form>
---
+++ src/container/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Titles from '../../components/Titles/Titles'
+import './Portfolio.css'
+const Portfolio = () => {
+  return (
+    <div className='portfolio' id='portfolio'>
+      <div className='portfolio-title-background'>
+        <Titles title={'PORTFOLIO'} />
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
